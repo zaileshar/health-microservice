fb16791905a1dc60afee0f2135d3ad9333b47cdb
zaileshar <zaileshar@gmail.com>
2021-03-19 13:10:18 +0530

    fix memory leak

---
+++ hal_20c2.v
@@ -0,0 +1,19 @@
+module hal #(parameter WIDTH=8) (
+    input clk,
+    input rst_n,
+    input [WIDTH-1:0] din,
+    input valid_in,
+    output reg [WIDTH-1:0] dout,
+    output reg valid_out
+);
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            dout <= 0;
+            valid_out <= 0;
+        end else begin
+            if (valid_in)
+                dout <= din + 1'b1;
+            valid_out <= valid_in;
+        end
+    end
+endmodule
