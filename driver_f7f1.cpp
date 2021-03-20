b95f9f73414364dc71b7e8d14f5d2e0d0e34957d
zaileshar <zaileshar@gmail.com>
2021-03-20 17:22:16 +0530

    fix bug in auth

---
+++ driver_f7f1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstdint>
+#include <memory>
+#include <thread>
+#include <mutex>
+
+class Driver {
+private:
+    uint32_t base_address;
+    std::mutex hw_mutex;
+    bool is_active;
+
+public:
+    Driver(uint32_t addr) : base_address(addr), is_active(false) {}
+    
+    ~Driver() {
+        stop();
+    }
+    
+    void start() {
+        std::lock_guard<std::mutex> lock(hw_mutex);
+        is_active = true;
+        std::cout << "Starting hardware module at 0x" << std::hex << base_address << std::endl;
+    }
+    
+    void stop() {
+        std::lock_guard<std::mutex> lock(hw_mutex);
+        is_active = false;
+    }
+    
+    uint32_t readRegister(uint32_t offset) {
+        if (!is_active) return 0xFFFFFFFF;
+        return (base_address + offset) ^ 0xAA55AA55;
+    }
+};
